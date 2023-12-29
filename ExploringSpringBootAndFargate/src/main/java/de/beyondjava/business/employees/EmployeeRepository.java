c67b8c0b604937311988526afed00a8dc0596bcf
Jason Camp <jasoncamp12@gmail.com>
2023-12-28 21:39:40 -0700

    cdk v2

--- ExploringSpringBootAndFargate/src/main/java/de/beyondjava/business/employees/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package de.beyondjava.business.employees;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-    List<Employee> findEmployeeByFirstNameContaining(String start);
-}
--- reactive-programming/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
--- CREATE DATABASE reactive;
-
-CREATE TABLE IF NOT EXISTS employee
-(
-    id SERIAL PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL
-);
